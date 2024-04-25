6c4f28202cfdc1dc78e9a4895462b96d0469dff2
Pedro Lima <eng.pedrocarim@gmail.com>
2024-04-25 16:01:53 -0300

    Remove errorBoundry.

--- src/router.jsx
+++ src/router.jsx
@@ -4,18 +4,16 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Projects from './pages/Projects'
 import Skills from './pages/Skills'
-import RouteBoundary from './error-boundaries/RouteBoundary'
 
 const router = createBrowserRouter ([
     {
         path: "/portifolio",
         element: <RootLayout />,
-        errorElement: <RouteBoundary />,
         children: [
             {index: true, element: <Home />},
-            {path: "/portifolio/about", element: <About />, errorElement: <RouteBoundary />},
-            {path: "/portifolio/projects", element: <Projects />, errorElement: <RouteBoundary />},
-            {path: "/portifolio/skills", element: <Skills />, errorElement: <RouteBoundary />}
+            {path: "/portifolio/about", element: <About />},
+            {path: "/portifolio/projects", element: <Projects />},
+            {path: "/portifolio/skills", element: <Skills />}
         ]
     }
 ])
