ff9f75378d1a2eb5e4fb0c42e811a13d8a320f96
Pedro Lima <eng.pedrocarim@gmail.com>
2024-04-25 16:41:45 -0300

    Configuration of vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Configuração do middleware para redirecionar todas as solicitações para o index.html
+    proxy: {
+      '/': {
+        target: 'https://striffer.github.io/portifolio/', // Substitua pelo endereço do seu servidor
+        bypass: (req, res) => {
+          if (req.headers.accept.indexOf('html') !== -1) {
+            return '/index.html';
+          }
+        },
+      },
+    },
+  },
   base: "/portifolio",
 })
